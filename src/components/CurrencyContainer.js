5abdb752318d58496be3bfa40456292af1c0f4aa
Allan Hall <bigal76126@gmail.com>
2019-05-22 07:06:36 -0400

    successfly fetched data, errors in trying to display it on page

--- src/components/CurrencyContainer.js
+++ src/components/CurrencyContainer.js
@@ -4,16 +4,10 @@ class CurrencyContainer extends Component {
   render() {
     return (
       <>
-        <div className="name">
-          This currency name will go using 'this.prop.'
-        </div>
-        <div className="price">
-          This is where the price will go: quotes->usd->price{' '}
-        </div>
-        <div className="symbol">
-          This is where the symbol will go: quotes->symbol
-        </div>
-        <div className="rank">This is where the rank will go: quotes->rank</div>
+        <h3 className="name">{this.props.name}</h3>
+        <div className="price">{this.props.quotes.USD.price}</div>
+        <div className="symbol">{this.props.quotes.symbol}</div>
+        <div className="rank">{this.props.quotes.rank}</div>
       </>
     )
   }
--- src/components/CurrencyList.js
+++ src/components/CurrencyList.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react'
 import CurrencyContainer from './CurrencyContainer'
 
 class CurrencyList extends Component {
-  render() {
-    return <CurrencyContainer />
+  state = {
+    currencyData: []
   }
   componentDidMount() {
-    this.getDataFromApi()
-  }
-  getDataFromApi = () => {
     fetch('https://api.coinmarketcap.com/v2/ticker/?limit=20')
       .then(resp => {
         return resp.json()
       })
       .then(json => {
         console.log(json)
+        this.setState({
+          currencyData: json.data
+        })
       })
+  }
+  render() {
+    return (
+      <main className="currency-list">
+        {this.state.currencyData.map(currency => {
+          return (
+            <CurrencyContainer
+              name={currency.name}
+              price={currency.quotes.USD.price}
+              symbol={currency.symbol}
+              rank={currency.quotes.rank}
+            />
+          )
+        })}
+      </main>
+    )
   }
 }
 
